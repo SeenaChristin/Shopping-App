b5df26e910020d49432e24f34abf9566dd7ae09c
Seena Christin <seenachristin@Seenas-MacBook-Air.local>
2023-11-24 21:12:41 +0530

    Fixed lint error

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,7 +2,6 @@ import React from "react";
 import Header from "./Header";
 import MenuBar from "./MenuBar";
 import { Outlet } from "react-router-dom";
-import { useParams } from "react-router-dom";
 import { useContext } from "react";
 import ProductContext from "../utils/ProductContext";
 
